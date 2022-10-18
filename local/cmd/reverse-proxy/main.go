10fd7c3ea575f513a456b714e26a8eb749a69a19
Lionel Villard <villard@us.ibm.com>
2022-10-18 13:29:58 -0400

    fix channel

--- local/cmd/reverse-proxy/main.go
+++ local/cmd/reverse-proxy/main.go
@@ -15,11 +15,25 @@ func main() {
 
     handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
         return func(w http.ResponseWriter, r *http.Request) {
+            //body, err := io.ReadAll(r.Body)
+            //if err != nil {
+            //    log.Println(err)
+            //    return
+            //}
+            //r2 := r.Clone(r.Context())
+            //
+            //r.Body = io.NopCloser(bytes.NewReader(body))
+            //r2.Body = io.NopCloser(bytes.NewReader(body))
+            //
+            //r.ParseForm()
+            //
+            //rec := httptest.NewRecorder()
+            //
             //log.Printf("proxying request to /knative-sources/slack-direct")
             //r.Host = "github-adapter.knative-sources.127.0.0.1.sslip.io"
             //r.URL.Path = "/default/slack-direct"
             //
-            //p.ServeHTTP(w, r)
+            //p.ServeHTTP(rec, r2)
 
             log.Printf("proxying request to /knative-sources/slack-persisted")
             r.Host = "github-adapter.knative-sources.127.0.0.1.sslip.io"
